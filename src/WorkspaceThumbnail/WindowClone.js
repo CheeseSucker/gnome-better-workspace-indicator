1af9b84d22ed544dfcf56ab81d77d9027b57fe68
CheeseSucker <cheesesucker@gmail.com>
2022-10-02 01:08:00 +0200

    Removed some unnecessary functionality from WorkspaceThumbnail

--- src/WorkspaceThumbnail/WindowClone.js
+++ src/WorkspaceThumbnail/WindowClone.js
@@ -1,26 +1,15 @@
 const { Clutter, GObject } = imports.gi;
 
-const DND = imports.ui.dnd;
-const Workspace = imports.ui.workspace;
-
 const PrimaryActorLayout = imports.misc.extensionUtils.getCurrentExtension().imports.src.WorkspaceThumbnail.PrimaryActorLayout.PrimaryActorLayout;
 
-
-var WindowClone = GObject.registerClass({
-    Signals: {
-        'drag-begin': {},
-        'drag-cancelled': {},
-        'drag-end': {},
-        'selected': { param_types: [GObject.TYPE_UINT] },
-    },
-}, class WindowClone extends Clutter.Actor {
+var WindowClone = GObject.registerClass(
+class WindowClone extends Clutter.Actor {
     _init(realWindow) {
         let clone = new Clutter.Clone({ source: realWindow });
         super._init({
             layout_manager: new PrimaryActorLayout(clone),
-            reactive: true,
+            reactive: false,
         });
-        this._delegate = this;
 
         this.add_child(clone);
         this.realWindow = realWindow;
@@ -35,18 +24,6 @@ var WindowClone = GObject.registerClass({
                 this.destroy();
             }, this);
         this._onPositionChanged();
-
-        this.connect('destroy', this._onDestroy.bind(this));
-
-        this._draggable = DND.makeDraggable(this, {
-            restoreOnSuccess: true,
-            dragActorMaxSize: Workspace.WINDOW_DND_SIZE,
-            dragActorOpacity: Workspace.DRAGGING_WINDOW_OPACITY,
-        });
-        this._draggable.connect('drag-begin', this._onDragBegin.bind(this));
-        this._draggable.connect('drag-cancelled', this._onDragCancelled.bind(this));
-        this._draggable.connect('drag-end', this._onDragEnd.bind(this));
-        this.inDrag = false;
 
         let iter = win => {
             let actor = win.get_compositor_private();
@@ -70,23 +47,11 @@ var WindowClone = GObject.registerClass({
         if (this._stackAbove == null)
             return null;
 
-        if (this.inDrag) {
-            if (this._stackAbove._delegate)
-                return this._stackAbove._delegate.getActualStackAbove();
-            else
-                return null;
-        } else {
-            return this._stackAbove;
-        }
+        return this._stackAbove;
     }
 
     setStackAbove(actor) {
         this._stackAbove = actor;
-
-        // Don't apply the new stacking now, it will be applied
-        // when dragging ends and window are stacked again
-        if (actor.inDrag)
-            return;
 
         let parent = this.get_parent();
         let actualAbove = this.getActualStackAbove();
@@ -120,60 +85,5 @@ var WindowClone = GObject.registerClass({
 
     _onPositionChanged() {
         this.set_position(this.realWindow.x, this.realWindow.y);
-    }
-
-    _onDestroy() {
-        this._delegate = null;
-
-        if (this.inDrag) {
-            this.emit('drag-end');
-            this.inDrag = false;
-        }
-    }
-
-    vfunc_button_press_event() {
-        return Clutter.EVENT_STOP;
-    }
-
-    vfunc_button_release_event(buttonEvent) {
-        this.emit('selected', buttonEvent.time);
-
-        return Clutter.EVENT_STOP;
-    }
-
-    vfunc_touch_event(touchEvent) {
-        if (touchEvent.type != Clutter.EventType.TOUCH_END ||
-            !global.display.is_pointer_emulating_sequence(touchEvent.sequence))
-            return Clutter.EVENT_PROPAGATE;
-
-        this.emit('selected', touchEvent.time);
-        return Clutter.EVENT_STOP;
-    }
-
-    _onDragBegin(_draggable, _time) {
-        this.inDrag = true;
-        this.emit('drag-begin');
-    }
-
-    _onDragCancelled(_draggable, _time) {
-        this.emit('drag-cancelled');
-    }
-
-    _onDragEnd(_draggable, _time, _snapback) {
-        this.inDrag = false;
-
-        // We may not have a parent if DnD completed successfully, in
-        // which case our clone will shortly be destroyed and replaced
-        // with a new one on the target workspace.
-        let parent = this.get_parent();
-        if (parent !== null) {
-            if (this._stackAbove == null)
-                parent.set_child_below_sibling(this, null);
-            else
-                parent.set_child_above_sibling(this, this._stackAbove);
-        }
-
-
-        this.emit('drag-end');
     }
 });
--- src/WorkspaceThumbnail/WorkspaceThumbnail.js
+++ src/WorkspaceThumbnail/WorkspaceThumbnail.js
@@ -1,40 +1,15 @@
 const { Clutter, GLib, GObject, Graphene, St } = imports.gi;
 
-const DND = imports.ui.dnd;
 const Main = imports.ui.main;
-const Util = imports.misc.util;
 const BackgroundManager = imports.ui.background.BackgroundManager;
 
 const WindowClone = imports.misc.extensionUtils.getCurrentExtension().imports.src.WorkspaceThumbnail.WindowClone.WindowClone;
 
-var ThumbnailState = {
-    NEW:            0,
-    EXPANDING:      1,
-    EXPANDED:       2,
-    ANIMATING_IN:   3,
-    NORMAL:         4,
-    REMOVING:       5,
-    ANIMATING_OUT:  6,
-    ANIMATED_OUT:   7,
-    COLLAPSING:     8,
-    DESTROYED:      9,
-};
-
 /**
  * @metaWorkspace: a #Meta.Workspace
  */
-var WorkspaceThumbnail = GObject.registerClass({
-    Properties: {
-        'collapse-fraction': GObject.ParamSpec.double(
-            'collapse-fraction', 'collapse-fraction', 'collapse-fraction',
-            GObject.ParamFlags.READWRITE,
-            0, 1, 0),
-        'slide-position': GObject.ParamSpec.double(
-            'slide-position', 'slide-position', 'slide-position',
-            GObject.ParamFlags.READWRITE,
-            0, 1, 0),
-    },
-}, class WorkspaceThumbnail extends St.Widget
+var WorkspaceThumbnail = GObject.registerClass(
+class WorkspaceThumbnail extends St.Widget
 {
     _init(metaWorkspace, monitorIndex) {
         super._init({
@@ -42,7 +17,6 @@ var WorkspaceThumbnail = GObject.registerClass({
             style_class: 'workspace-thumbnail',
             pivot_point: new Graphene.Point({ x: 0.5, y: 0.5 }),
         });
-        this._delegate = this;
 
         this.metaWorkspace = metaWorkspace;
         this.monitorIndex = monitorIndex;
@@ -85,10 +59,6 @@ var WorkspaceThumbnail = GObject.registerClass({
             'window-entered-monitor', this._windowEnteredMonitor.bind(this),
             'window-left-monitor', this._windowLeftMonitor.bind(this), this);
 
-        this.state = ThumbnailState.NORMAL;
-        this._slidePosition = 0; // Fully slid in
-        this._collapseFraction = 0; // Not collapsed
-
         this._bgManager = new BackgroundManager({
             monitorIndex: monitorIndex,
             container: this._contents,
@@ -102,7 +72,7 @@ var WorkspaceThumbnail = GObject.registerClass({
     }
 
     _lookupIndex(metaWindow) {
-        return this._windows.findIndex(w => w.metaWindow == metaWindow);
+        return this._windows.findIndex(w => w.metaWindow === metaWindow);
     }
 
     syncStacking(stackIndices) {
@@ -117,35 +87,6 @@ var WorkspaceThumbnail = GObject.registerClass({
             const previousClone = this._windows[i - 1];
             clone.setStackAbove(previousClone);
         }
-    }
-
-    set slidePosition(slidePosition) {
-        if (this._slidePosition == slidePosition)
-            return;
-
-        const scale = Util.lerp(1, 0.75, slidePosition);
-        this.set_scale(scale, scale);
-        this.opacity = Util.lerp(255, 0, slidePosition);
-
-        this._slidePosition = slidePosition;
-        this.notify('slide-position');
-        this.queue_relayout();
-    }
-
-    get slidePosition() {
-        return this._slidePosition;
-    }
-
-    set collapseFraction(collapseFraction) {
-        if (this._collapseFraction == collapseFraction)
-            return;
-        this._collapseFraction = collapseFraction;
-        this.notify('collapse-fraction');
-        this.queue_relayout();
-    }
-
-    get collapseFraction() {
-        return this._collapseFraction;
     }
 
     _doRemoveWindow(metaWin) {
@@ -166,7 +107,7 @@ var WorkspaceThumbnail = GObject.registerClass({
             let id = GLib.idle_add(GLib.PRIORITY_DEFAULT, () => {
                 if (!this._removed &&
                     metaWin.get_compositor_private() &&
-                    metaWin.get_workspace() == this.metaWorkspace)
+                    metaWin.get_workspace() === this.metaWorkspace)
                     this._doAddWindow(metaWin);
                 return GLib.SOURCE_REMOVE;
             });
@@ -182,7 +123,7 @@ var WorkspaceThumbnail = GObject.registerClass({
 
         // We might have the window in our list already if it was on all workspaces and
         // now was moved to this workspace
-        if (this._lookupIndex(metaWin) != -1)
+        if (this._lookupIndex(metaWin) !== -1)
             return;
 
         if (!this._isMyWindow(win))
@@ -213,7 +154,7 @@ var WorkspaceThumbnail = GObject.registerClass({
 
     _windowRemoved(metaWorkspace, metaWin) {
         let index = this._allWindows.indexOf(metaWin);
-        if (index != -1) {
+        if (index !== -1) {
             metaWin.disconnectObject(this);
             this._allWindows.splice(index, 1);
         }
@@ -222,12 +163,12 @@ var WorkspaceThumbnail = GObject.registerClass({
     }
 
     _windowEnteredMonitor(metaDisplay, monitorIndex, metaWin) {
-        if (monitorIndex == this.monitorIndex)
+        if (monitorIndex === this.monitorIndex)
             this._doAddWindow(metaWin);
     }
 
     _windowLeftMonitor(metaDisplay, monitorIndex, metaWin) {
-        if (monitorIndex == this.monitorIndex)
+        if (monitorIndex === this.monitorIndex)
             this._doRemoveWindow(metaWin);
     }
 
@@ -263,7 +204,7 @@ var WorkspaceThumbnail = GObject.registerClass({
     _isMyWindow(actor) {
         let win = actor.meta_window;
         return win.located_on_workspace(this.metaWorkspace) &&
-            (win.get_monitor() == this.monitorIndex);
+            (win.get_monitor() === this.monitorIndex);
     }
 
     // Tests if @win should be shown in the Overview
@@ -276,18 +217,6 @@ var WorkspaceThumbnail = GObject.registerClass({
     _addWindowClone(win) {
         let clone = new WindowClone(win);
 
-        clone.connect('selected', (o, time) => {
-            this.activate(time);
-        });
-        clone.connect('drag-begin', () => {
-            Main.overview.beginWindowDrag(clone.metaWindow);
-        });
-        clone.connect('drag-cancelled', () => {
-            Main.overview.cancelledWindowDrag(clone.metaWindow);
-        });
-        clone.connect('drag-end', () => {
-            Main.overview.endWindowDrag(clone.metaWindow);
-        });
         clone.connect('destroy', () => {
             this._removeWindowClone(clone.metaWindow);
         });
@@ -312,67 +241,11 @@ var WorkspaceThumbnail = GObject.registerClass({
     }
 
     activate(time) {
-        if (this.state > ThumbnailState.NORMAL)
-            return;
-
         // a click on the already current workspace should go back to the main view
         if (this.metaWorkspace.active)
             Main.overview.hide();
         else
             this.metaWorkspace.activate(time);
-    }
-
-    // Draggable target interface used only by ThumbnailsBox
-    handleDragOverInternal(source, actor, time) {
-        if (source == Main.xdndHandler) {
-            this.metaWorkspace.activate(time);
-            return DND.DragMotionResult.CONTINUE;
-        }
-
-        if (this.state > ThumbnailState.NORMAL)
-            return DND.DragMotionResult.CONTINUE;
-
-        if (source.metaWindow &&
-            !this._isMyWindow(source.metaWindow.get_compositor_private()))
-            return DND.DragMotionResult.MOVE_DROP;
-        if (source.app && source.app.can_open_new_window())
-            return DND.DragMotionResult.COPY_DROP;
-        if (!source.app && source.shellWorkspaceLaunch)
-            return DND.DragMotionResult.COPY_DROP;
-
-        return DND.DragMotionResult.CONTINUE;
-    }
-
-    acceptDropInternal(source, actor, time) {
-        if (this.state > ThumbnailState.NORMAL)
-            return false;
-
-        if (source.metaWindow) {
-            let win = source.metaWindow.get_compositor_private();
-            if (this._isMyWindow(win))
-                return false;
-
-            let metaWindow = win.get_meta_window();
-            Main.moveWindowToMonitorAndWorkspace(metaWindow,
-                this.monitorIndex, this.metaWorkspace.index());
-            return true;
-        } else if (source.app && source.app.can_open_new_window()) {
-            if (source.animateLaunchAtPos)
-                source.animateLaunchAtPos(actor.x, actor.y);
-
-            source.app.open_new_window(this.metaWorkspace.index());
-            return true;
-        } else if (!source.app && source.shellWorkspaceLaunch) {
-            // While unused in our own drag sources, shellWorkspaceLaunch allows
-            // extensions to define custom actions for their drag sources.
-            source.shellWorkspaceLaunch({
-                workspace: this.metaWorkspace.index(),
-                timestamp: time,
-            });
-            return true;
-        }
-
-        return false;
     }
 
     setScale(scaleX, scaleY) {
